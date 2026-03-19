ffc08eeb505a52a18da941e976c0d420603729de
paper-del <przemekpotwora127@gmail.com>
2026-03-19 11:14:59 +0100

    more bash

---
+++ printf.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+printf 'a\n' b c d
+printf '%s\n' b c d
+
---
+++ user_input.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p 'Username: ' uservar
+read -sp 'Passwird: ' passvar
+echo
+echo $uservar $passvar
+
+read -p 'podaj 3 marki aut: ' car1 car2 car3 
+echo $car1 $car2 $car3
